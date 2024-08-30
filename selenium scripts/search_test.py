1352042654c8f6059ee8e0180b5245563c7a1b07
zivhd <56124166+zivhd@users.noreply.github.com>
2024-08-30 13:21:53 +0800

    Update search_test.py

--- selenium scripts/search_test.py
+++ selenium scripts/search_test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,33 +17,23 @@ delay = 0.5
 
 def search(keyword):
     try:
-        search = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((
-                By.XPATH, 
-                "/html/body/div[1]/div[2]/div/div[1]/div[3]/div[1]/div/input"
-            ))
+        # Wait until the search input field is visible and clickable
+        search_input = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div//input[@placeholder='Search ' and contains(@class, 'text-xs')]"))
         )
-        search.click()
-        print("Clicked search")
-        search.send_keys(keyword)
-        print(f"Entered {keyword}")
-
-        search_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((
-                By.XPATH, 
-                "/html/body/div[1]/div[2]/div/div[1]/div[3]/div[1]/div/div[1]"
-            ))
-        )
-
-        search_button.click()
-        print("Clicked search button")
-
-
+        # Optionally clear the existing text if any
+        search_input.clear()
+        
+        # Send the search keyword followed by RETURN key
+        search_input.send_keys(keyword + Keys.RETURN)
+        
+        print("Search executed successfully.")
+        
     except Exception as e:
-        print(f"Failed to open hamburger menu due to: {e}")
+        print(f"Failed to execute search due to: {e}")
         return False
+    
     return True
-
 
     
 
